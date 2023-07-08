8a797c880febb0d2a9395a50567bbb7e406a2407
CarlosIgreda <cjigredarivas@gmail.com>
2023-07-08 00:50:40 -0500

    Fix linter error

--- capitalize_decorator.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'decorator'
-
-# CapitalizeDecorator
-class CapitalizeDecorator < Decorator
-  def correct_name
-    super.capitalize
-  end
-end
--- nameable.rb
+++
@@ -1,6 +0,0 @@
-# Interface
-class Nameable
-  def correct_name
-    raise NotImplementedError, "Subclasses must implement 'correct_name' method"
-  end
-end
